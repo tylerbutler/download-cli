2ed569ff89e71925c57ee48bb340d884a12f5aa1
Tyler Butler <tyler@tylerbutler.com>
2022-03-02 11:56:41 -0800

    fix audit issues

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "rules": {
+        "unicorn/prefer-module": "off",
+        "unicorn/prevent-abbreviations": "off",
+        "unicorn/no-array-reduce": "off",
+        "comma-dangle": "off",
+        "node/prefer-global/process": "off"
+    }
+}
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-import path from 'path';
-import execa from 'execa';
-import pathExists from 'path-exists';
-import rimraf from 'rimraf';
-import test from 'ava';
+const path = require('path');
+const execa = require('execa');
+const pathExists = require('path-exists');
+const rimraf = require('rimraf');
+const test = require('ava');
 
-test(async t => {
-	await execa.stdout(path.join(__dirname, 'cli.js'), ['--out', __dirname, 'https://github.com/kevva/download-cli/archive/master.zip']);
+test("Test downloading main branch tarball", async t => {
+	await execa.stdout(path.join(__dirname, 'cli.js'), ['--out', __dirname, 'https://github.com/tylerbutler/download-cli/archive/master.zip']);
 	t.true(pathExists.sync(path.join(__dirname, 'download-cli-master.zip')));
 	rimraf.sync(path.join(__dirname, 'download-cli-master.zip'));
 });
